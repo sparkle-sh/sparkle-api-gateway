bd494ed9a3263db48dc403866f71376d380238f4
Kacper Czyż <mrnek135@gmail.com>
2020-10-08 20:51:13 +0200

    Add connection pool support

---
+++ src/core/db.py
@@ -0,0 +1,29 @@
+import asyncpg
+import asyncio
+from core.log import get_logger
+
+log = get_logger("db")
+DSN = 'postgres://sparkle:foobar@localhost:5432/sparkledb'
+
+
+class ConnectionPool(object):
+    pool: asyncpg.pool.Pool = None
+
+    @staticmethod
+    async def init(dsn=DSN, tries=10, interval=5):
+        for _ in range(tries):
+            try:
+                ConnectionPool.pool = await asyncpg.create_pool(
+                    dsn=dsn
+                )
+                log.info("Database connection established succesfully")
+                return
+            # tODO: catch more precise exception
+            except Exception as e:
+                log.warning("Unable to connect to database, retrying in %ss", interval)
+            await asyncio.sleep(interval)
+        raise RuntimeError("Could not connect to database")
+
+    @staticmethod
+    def acquire_connection():
+        return ConnectionPool.pool.acquire()
