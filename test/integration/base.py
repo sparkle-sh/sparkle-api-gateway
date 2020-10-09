2d4de8615afe714a44c647bcc7bf51f17e09de55
nekoffski <mrnek135@gmail.com>
2020-10-09 10:53:39 +0200

    fix

--- test/integration/base.py
+++ test/integration/base.py
@@ -6,8 +6,11 @@ class ApiGatewayTestBase(TestBase):
     def setUp(self):
         super().setUp()
         if self.is_test_env():
+            self.start_database()
+            self.wait_for_database()
+
             self.start_api_gateway()
-            self.wait_for_api_gw()
+            self.wait_for_api_gateway()
 
     def tearDown(self):
         super().tearDown()
