5c273d09844372a95322487737e844fea0a166fc
nekoffski <mrnek135@gmail.com>
2020-10-09 12:18:43 +0200

    fix

--- test/integration/basic_tests.py
+++ test/integration/basic_tests.py
@@ -2,5 +2,11 @@ from base import ApiGatewayTestBase
 
 
 class BasicTests(ApiGatewayTestBase):
+    def setUp(self):
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+    
     def test_test(self):
         pass
