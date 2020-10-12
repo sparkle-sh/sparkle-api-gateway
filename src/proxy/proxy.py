35812451703cd5c415e2dae74c0b1f1b63fe844a
Kacper Czyż <mrnek135@gmail.com>
2020-10-12 20:00:27 +0200

    Foundations of midpoint proxy

--- src/proxy/proxy.py
+++ src/proxy/proxy.py
@@ -1,5 +1,5 @@
+from .midpoint_proxy import setup_midpoint_proxy
 
 
-
-async def setup_proxies(app):
-    pass
+async def setup_proxies(app, cfg):
+    app.blueprint(await setup_midpoint_proxy(cfg.midpoint))
