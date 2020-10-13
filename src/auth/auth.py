4daf6d8c1650361f3a11796c28094028f2f0cd53
Ania Walaszek <64088137+aniawala@users.noreply.github.com>
2020-10-13 21:57:37 +0200

    Add user credentials validation with database

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,13 +1,20 @@
 from sanic_jwt import exceptions
+from core.db import ConnectionPool
 
 
 async def authenticate(request, *args, **kwargs):
     username = request.json.get('username', None) 
     passwd = request.json.get('passwd', None)
 
-    if not username or not passwd:
-        raise exceptions.AuthenticationFailed("Missing username or password")
-    
+    async with ConnectionPool.acquire_connection() as conn:
+        query = f"SELECT passwd FROM users WHERE name='{username}'"
+        res = await conn.fetch(query)
+        if not res:
+            raise exceptions.AuthenticationFailed("Invalid username")
+        
+        if passwd != res[0]['passwd']:
+            raise exceptions.AuthenticationFailed("Invalid password")
+
     user = {'username': username, 'passwd': passwd}
     return user
 
