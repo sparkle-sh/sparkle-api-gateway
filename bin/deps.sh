e235b3b09ce3c8d0294eae038fb73a9432a45fb1
aniawala <anna.walaszek97@gmail.com>
2020-10-14 22:14:15 +0200

    Add remotely creating db user, password hashing

--- bin/deps.sh
+++ bin/deps.sh
@@ -3,6 +3,7 @@
 sudo apt-get install -y python3.7
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y python3.7-venv
+sudo apt-get install -y libpq-dev
 
 sudo python3.7 -m ensurepip
 sudo python3.7 -m pip install virtualenv
