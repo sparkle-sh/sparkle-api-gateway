0cecde6db2b4b3eb7c83c8a01d995c3c5ef5fdac
Ania Walaszek <64088137+aniawala@users.noreply.github.com>
2020-10-15 22:36:30 +0200

    Add --list option to create user script

--- misc/schema.sql
+++ misc/schema.sql
@@ -10,5 +10,9 @@ CREATE TABLE users (
     passwd VARCHAR(256)
 );
 
+INSERT INTO users (name, passwd) VALUES ('user1', 'passwd1');
+INSERT INTO users (name, passwd) VALUES ('user2', 'passwd2');
+
+
 GRANT ALL PRIVILEGES ON TABLE users TO sparkle;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO sparkle;
