f4b9b458bed8a611a52811c1f5743612e71e8531
aniawala <anna.walaszek97@gmail.com>
2020-10-16 16:44:39 +0200

    Add username validation

--- misc/schema.sql
+++ misc/schema.sql
@@ -10,9 +10,6 @@ CREATE TABLE users (
     passwd VARCHAR(256)
 );
 
-INSERT INTO users (name, passwd) VALUES ('user1', 'passwd1');
-INSERT INTO users (name, passwd) VALUES ('user2', 'passwd2');
-
 
 GRANT ALL PRIVILEGES ON TABLE users TO sparkle;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO sparkle;
