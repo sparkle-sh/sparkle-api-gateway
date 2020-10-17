f8911da76dec54c6b1129a5fd5d2a130abda9fa4
Ania Walaszek <64088137+aniawala@users.noreply.github.com>
2020-10-17 13:07:28 +0200

    Add passwd check

--- bin/deps.sh
+++ bin/deps.sh
@@ -3,6 +3,8 @@
 sudo apt-get install -y python3.7
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y python3.7-venv
+sudo apt-get install -y build-essential
+sudo apt-get install -y python3.7-dev
 sudo apt-get install -y libpq-dev
 
 sudo python3.7 -m ensurepip
