93c2a5240a1c27dac3dd0e1d7425272a37f1c235
Ania Walaszek <64088137+aniawala@users.noreply.github.com>
2020-10-20 19:56:34 +0200

    Add integration tests

--- test/integration/base.py
+++ test/integration/base.py
@@ -1,16 +1,15 @@
 import os
 from sparkle_test_base.base import TestBase
+from sparkle_test_base.config import API_GW_API_BASE
 
 
 class ApiGatewayTestBase(TestBase):
     def setUp(self):
         super().setUp()
+        self.url = API_GW_API_BASE
         if self.is_test_env():
             self.start_database()
             self.wait_for_database()
 
             self.start_api_gateway()
             self.wait_for_api_gateway()
-
-    def tearDown(self):
-        super().tearDown()
