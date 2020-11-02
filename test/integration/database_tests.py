d341f1634122180705647eaf7df187f35e893db4
aniawala <anna.walaszek97@gmail.com>
2020-11-02 20:32:31 +0100

    Add db tests

--- test/integration/database_tests.py
+++ test/integration/database_tests.py
@@ -1,12 +1,15 @@
 from sparkle_test_base.base import TestBase
+from src.core import config
 
 
 class DatabaseTests(TestBase):
     def setUp(self):
         super().setUp()
+        self.config = config.Config("./cfg/config.json")
+
         if self.is_test_env():
             self.start_database()
-            self.wait_for_database()
+        self.wait_for_database()
 
     def tearDown(self):
         super().tearDown()
@@ -14,7 +17,7 @@ class DatabaseTests(TestBase):
 
     def test_check_tables(self):
         cursor = self.get_db_cursor()
-        cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+        cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
         tables = cursor.fetchall()
         self.assertEqual(tables[0][0], "users")
-
