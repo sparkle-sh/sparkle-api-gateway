c8d2041753874d83e93b666560c60e0112d5a864
nekoffski <mrnek135@gmail.com>
2020-11-03 17:09:50 +0100

    Migrated db to another repo

--- bin/run_integration_tests.sh
+++ bin/run_integration_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ./venv/bin/activate
-MISC=$(pwd)/misc PYTHONPATH=./test/integration ./venv/bin/py.test ./test/integration/*tests.py --verbose --disable-pytest-warnings
+MISC=$(pwd)/test_misc PYTHONPATH=./test/integration ./venv/bin/py.test ./test/integration/*tests.py --verbose --disable-pytest-warnings
